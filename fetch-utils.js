966a4b2a6621cdcb8e5bbc67f7b6f56fae4f7d1d
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-08-16 11:54:40 -0700

    connected to supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://geeqdoksbskhfiqfdfhh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZXFkb2tzYnNraGZpcWZkZmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA2NzQ4NjUsImV4cCI6MTk3NjI1MDg2NX0.uUy4ro2um6VQlmmAhdX_cl6hW4MYKynIulse6QLIhCs';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
